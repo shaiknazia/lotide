83ac7091738a48df25ad980741bce690477ad15b
Nazia Shaik <shaiknazia.n@gmail.com>
2020-03-09 19:52:43 +0000

    mocha asset equal test ifles

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,13 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertEqual = require('../assertEqual');
 
-assertEqual((middle([1, 2, 3])),`Middle is 2`)// => [2]
-assertEqual((middle([1, 2, 3, 4, 5])), `Middle is 3`)
 
+
+describe("#Middle", () => {
+  it("Middle is 2", () => {
+    assert.deepEqual((middle([1, 2, 3])),`Middle is 2` )
+  });
+  it("Middle is 3", () => {
+    assert.deepEqual((middle([1, 2, 3, 4, 5])), `Middle is 3` )
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,11 @@
+
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
 
+describe("#tail", () => {
+  it("returns true if both arrays are equal", () => {
+    assert.deepEqual((tail(words)),["Lighthouse", "Labs"] )
+  });
+});
